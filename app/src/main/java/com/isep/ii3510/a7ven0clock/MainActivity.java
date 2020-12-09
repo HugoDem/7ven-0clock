d024f919a32cc158080109ccd032084a382a1893
HugoDem <hugo.demarcq@gmail.com>
2020-12-09 09:47:13 +0100

    Init Project

---
+++ app/src/main/java/com/isep/ii3510/a7ven0clock/MainActivity.java
@@ -0,0 +1,17 @@
+package com.isep.ii3510.a7ven0clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
