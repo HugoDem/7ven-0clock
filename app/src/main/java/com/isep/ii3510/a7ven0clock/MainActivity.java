351b3b72b26b05f7985b7ad5badc3c883282cb3b
HugoDem <hugo.demarcq@gmail.com>
2020-12-12 19:47:27 +0100

    Logo added, fragments added, bottomNavigationView working

--- app/src/main/java/com/isep/ii3510/a7ven0clock/MainActivity.java
+++ app/src/main/java/com/isep/ii3510/a7ven0clock/MainActivity.java
@@ -1,17 +1,71 @@
 package com.isep.ii3510.a7ven0clock;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Typeface;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
+
+import android.text.format.Time;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        loadFragments(new ClockFragment());
 
     }
+
+
+    public Boolean onNavigationItemSelected(@NonNull MenuItem item){
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+            case R.id.navigation_clock:
+                fragment = new ClockFragment();
+                break;
+            case R.id.navigation_alarm:
+                fragment = new AlarmFragment();
+                break;
+            case R.id.navigation_timer:
+                fragment = new TimerFragment();
+                break;
+            case R.id.navigation_settings:
+                fragment = new SettingsFragment();
+                break;
+        }
+
+        return loadFragments(fragment);
+    }
+
+    private boolean loadFragments(Fragment fragment){
+        if(fragment == null){
+            return false;
+        }
+        else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/isep/ii3510/a7ven0clock/MainFragment.java
@@ -0,0 +1,12 @@
+package com.isep.ii3510.a7ven0clock;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    public void updateDesign(String request){
+
+    }
+
+
+}
