40a0f8e175bfb591861a583c27ec3a48033b92f2
Talha Tunc <talha.tunc@isep.fr>
2021-01-05 23:35:00 +0100

    First modif AlarmFragment

--- app/src/main/java/com/isep/ii3510/a7ven0clock/AlarmFragment.java
+++ app/src/main/java/com/isep/ii3510/a7ven0clock/AlarmFragment.java
@@ -1,12 +1,11 @@
 package com.isep.ii3510.a7ven0clock;
-
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextClock;
+import android.widget.TimePicker;
+import java.util.Timer;
+import.java.Timertask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +13,61 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AlarmFragment extends Fragment {
+
+    TimePicker alarmTime;
+    TextClock currentTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        alarmTime = findViewById(R.id.timePicker);
+        currentTime = findViewById(R.id.textClock);
+        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (currentTime.getText().toString().equals(AlarmTime())){
+                    r.play();
+                }else{
+                    r.stop();
+                }
+            }
+        }, 0, 1000);
+    }
+
+
+    public String AlarmTime(){
+
+        Integer alarmHours = alarmTime.getCurrentHour();
+        Integer alarmMinutes = alarmTime.getCurrentMinute();
+        String stringAlarmMinutes;
+
+        if (alarmMinutes<10){
+            stringAlarmMinutes = “0”;
+            stringAlarmMinutes = stringAlarmMinutes.concat(alarmMinutes.toString());
+        }else{
+            stringAlarmMinutes = alarmMinutes.toString();
+        }
+        String stringAlarmTime;
+
+        if(alarmHours>12){
+            alarmHours = alarmHours – 12;
+            stringAlarmTime = alarmHours.toString().concat(“:”).concat(stringAlarmMinutes).concat(” PM”);
+        }else{
+            stringAlarmTime = alarmHours.toString().concat(“:”).concat(stringAlarmMinutes).concat(” AM”);
+        }
+        return stringAlarmTime;
+    }
+}
+            }
+
+
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
